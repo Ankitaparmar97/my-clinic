d349472aa5b0ef05e5cb3f793d132d46ac8d2335
Darshan Kapadiya <darshankapadiya9@gmail.com>
2025-04-18 15:13:21 +0530

    init

---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ padding: '20px 0px', backgroundColor: 'blacko', color: 'white' }}>
+      <h1>My App Header</h1>
+    </header>
+  );
+};
+
+export default Header;
