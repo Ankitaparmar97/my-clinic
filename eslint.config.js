18491d1dd7563d1e5f20310b2049c69ea17b949f
Darshan Kapadiya <darshankapadiya9@gmil.com>
2025-06-29 03:48:48 +0530

    unnecessary dependency removed makefile and documentation added

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,38 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier';
 import checkFile from 'eslint-plugin-check-file';
+import reactX from 'eslint-plugin-react-x';
 
 export default tseslint.config([
   tseslint.config({
     ignores: ['dist', 'node_modules', '.vscode', '*.json', '*.lock'],
   }),
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      reactX.configs['recommended-typescript'],
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       'check-file': checkFile,
       prettier,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'prettier/prettier': 'warn',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          vars: 'all',
-          args: 'after-used',
-          ignoreRestSiblings: true,
-          varsIgnorePattern: '^_',
-          argsIgnorePattern: '^_',
-        },
-      ],
-      'no-unused-vars': 'off',
 
       // File name must be PascalCase
       'check-file/filename-naming-convention': [
@@ -47,13 +42,16 @@ export default tseslint.config([
         },
       ],
 
-      // Folder naming convention must be cabab-case
+      // Folder naming convention must be kebab-case
       'check-file/folder-naming-convention': [
         'error',
         {
           '**/': 'KEBAB_CASE',
         },
       ],
+
+      // Disable fallback rules that duplicate TS-aware versions
+      'no-unused-vars': 'off',
     },
   },
   {
