cf1cc5f38dfe5fad1e9b0584ab8ccf20a78c8997
Darshan Kapadiya <darshankapadiya9@gmil.com>
2025-06-28 23:47:48 +0530

    linter, prettier added

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import checkFile from 'eslint-plugin-check-file';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -16,13 +18,47 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'check-file': checkFile,
+      prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true,
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'no-unused-vars': 'off',
+
+      // File name must be PascalCase
+      'check-file/filename-naming-convention': [
+        'error',
+        {
+          '**/*.{ts,tsx}': 'PASCAL_CASE',
+          '**/{index,main,abc}.{ts,tsx}': 'CAMEL_CASE', // allow index.tsx to be lowercase
+        },
+      ],
+
+      // Folder naming convention must be cabab-case
+      'check-file/folder-naming-convention': [
+        'error',
+        {
+          '**/': 'KEBAB_CASE',
+        },
       ],
     },
   },
-)
+  {
+    files: ['**/index.tsx', '**/main.tsx'],
+    rules: {
+      'check-file/filename-naming-convention': 'off',
+    },
+  },
+);
