d50317d6ec8a9767ff180c82c73583d886d1818f
Ankita Parmar <ankitaparmar18697@gmail.com>
2026-05-11 18:43:45 +0530

    fix: dependency issue due to carosol and react version mis match testing

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,8 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { useTheme } from '@mui/material/styles';
 import { CONTACT } from '../constants';
-//#TODO: make generalised button for all buttons on website
+//#TODO: Update and use latest carousal and then delete tomal file
+
 
 const PhoneCall = () => {
   const theam = useTheme();
